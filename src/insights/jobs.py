dde5f56c64c8e3790590d8bd2c7f0aae402255ed
Rafael <rafaelgermanio@gmail.com>
2024-05-03 14:26:17 -0300

    feat: Requisito 04

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,10 +13,21 @@ class ProcessJobs:
             return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
-        job_types = []
-        for type in self.jobs_list:
-            job_types.append(type["job_type"])
-        return set(job_types)
+        job_types = {
+            type["job_type"]
+            for type in self.jobs_list
+        }
+        return job_types
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+    def filter_by_multiple_criteria(
+            self, jobs: List[Dict], filter_criteria: Dict
+            ) -> List[Dict]:
+        if not isinstance(filter_criteria, dict):
+            raise TypeError(f"{filter_criteria} is  not a dictionary")
+        jobs_filtered = [
+            job
+            for job in jobs
+            if all(job[key] == value for key, value in filter_criteria.items())
+            ]
+
+        return jobs_filtered
